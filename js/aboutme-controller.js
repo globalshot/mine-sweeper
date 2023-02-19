8182eb1c632442fbf38bddd893b13c490a50c57c
globalshot <31039763+globalshot@users.noreply.github.com>
2023-02-19 19:41:47 +0200

    original will be reuploaded

--- js/aboutme-controller.js
+++
@@ -1,9 +0,0 @@
-//idk how much rlly i need this, maximum imma delete
-
-function toggleAboutMe() {
-    let aboutMe = document.querySelector('div.aboutMe')
-    aboutMe.classList.toggle('hidden')
-
-    let gallery = document.querySelector('div.modal')
-    gallery.classList.add('hidden')
-}
